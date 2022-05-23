eaf8aa576ca6dd0f090db2e61ce0821a33978b20
yingjun <chen787263351@163.com>
2022-05-23 19:31:14 +0800

    把本地工程添加到git

---
+++ src/HelloGit.java
@@ -0,0 +1,18 @@
+public class HelloGit {
+    public static void main(String[] args) {
+        System.out.println("hello,git");
+        System.out.println("hello,git");
+        System.out.println("hello,git");
+        System.out.println("hello,git");
+        System.out.println("hello,git");
+        System.out.println("hello,git");
+        System.out.println("hello,git");
+        System.out.println("hello,git");
+    }
+    public void test(){
+        System.out.println("kkkk");
+        System.out.println("kkkk");
+        System.out.println("kkkk");
+        System.out.println("kkkk");
+    }
+}
